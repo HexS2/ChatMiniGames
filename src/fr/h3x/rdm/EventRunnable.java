62f116d107f374336942f170d7e169f3c8bba739
HexS2 <inoxsdev@gmail.com>
2019-03-07 14:00:50 +0100

    remove static usage

--- src/fr/h3x/rdm/EventRunnable.java
+++ src/fr/h3x/rdm/EventRunnable.java
@@ -7,11 +7,18 @@ import org.bukkit.scheduler.BukkitRunnable;
 import java.util.Random;
 
 public class EventRunnable extends BukkitRunnable {
+
+    private Main plugin;
+
+    public EventRunnable(Main plugin) {
+        this.plugin = plugin;
+    }
+
     @Override
     public void run() {
-        Main.event = true;
-            Main.message = new Message(getRandomNumberInRange(5,15),"AbCdEFgHIjK^$LMnO!pQRStUVwXYZ@*").getRandomString();
-           Bukkit.broadcastMessage("§7[§6QatariaGame§7] §aJeu de rapidité : Le premier qui écrit §c"+Main.message+" §agagne 50$ !");
+        plugin.setStarted(true);
+            plugin.setMessage(new Message(getRandomNumberInRange(5,15),"AbCdEFgHIjK^$LMnO!pQRStUVwXYZ@*").getRandomString());
+           Bukkit.broadcastMessage("§7[§6QatariaGame§7] §aJeu de rapidité : Le premier qui écrit §c"+plugin.getMessage()+" §agagne 50$ !");
    }
 
     private static int getRandomNumberInRange(int min, int max) {
--- src/fr/h3x/rdm/Main.java
+++ src/fr/h3x/rdm/Main.java
@@ -10,15 +10,32 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 
 public class Main extends JavaPlugin implements Listener  {
-    static String message;
-    static boolean event = false;
+    private String message;
+    private boolean started = false;
     private static Economy econ = null;
     public void onEnable() {
         setupEconomy();
-        new EventRunnable().runTaskTimer(this,36000,36000);
+        new EventRunnable(this).runTaskTimer(this,150,150);
         Bukkit.getServer().getPluginManager().registerEvents(this,this);
 
     }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isStarted() {
+        return started;
+    }
+
+    public void setStarted(boolean started) {
+        this.started = started;
+    }
+
     private boolean setupEconomy() {
         if (getServer().getPluginManager().getPlugin("Vault") == null) {
             return false;
@@ -33,11 +50,11 @@ public class Main extends JavaPlugin implements Listener  {
 
     @EventHandler
     public void onPlayerChat(PlayerChatEvent e){
-    if(event == true){
+    if(isStarted()){
         if(e.getMessage().equals(message)){
             Main.econ.depositPlayer(e.getPlayer(),50);
             Bukkit.broadcastMessage("§7[§6QatariaGame§7] §aLe joueur §6"+e.getPlayer().getName()+" §aa été le plus rapide !");
-            event = false;
+            setStarted(false);
         }
     }
 }
